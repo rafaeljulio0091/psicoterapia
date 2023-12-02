b10126dc0c346a94b278a6427b2133997a8a4509
= <83626697+rafaeljulio0091@users.noreply.github.com>
2023-12-02 18:04:42 -0300

    atualização

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -2,11 +2,17 @@ function acceptCookies() {
     document.querySelector(".box-cookies").classList.add('hide');
     localStorage.setItem("pureJavaScriptCookies", "accept");
 }
+function rejectCookies() {
+    document.querySelector(".box-cookies").classList.add('hide');
+    localStorage.setItem("pureJavaScriptCookies", "reject");
+}
 $(document).ready(function(){
     if (!localStorage.pureJavaScriptCookies) {
         document.querySelector(".box-cookies").classList.remove('hide');
     }
-    let btnCookies = document.querySelector(".btn-cookies");
+    let btnCookiesAccept = document.querySelector(".btn-cookies-accept");
+    let btnCookiesReject = document.querySelector(".btn-cookies-reject");
       
-    btnCookies.addEventListener('click', acceptCookies);
+    btnCookiesAccept.addEventListener('click', acceptCookies);
+    btnCookiesReject.addEventListener('click', rejectCookies);
 })
